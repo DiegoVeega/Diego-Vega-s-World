df73f5218c41a52a22c6ccda9e00a26cc9c57b70
Diego Vega <00148816@uca.edu.sv>
2018-05-31 21:10:37 -0600

    Creacion de clase que implementa la interfaz de la Milicia

---
+++ src/Milicias/Especialista.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Milicias;
+
+/**
+ *
+ * @author diego
+ */
+public class Especialista implements Milicia{
+
+    @Override
+    public String atacar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String defender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
