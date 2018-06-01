78b9a9ef2d212ff801145ea172a040b6f2e347ee
Diego Vega <00148816@uca.edu.sv>
2018-05-31 21:09:21 -0600

    Creacion de clase que implementa la interfaz de la Edificacion

---
+++ src/Edificaciones/Recolector_Oro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edificaciones;
+
+/**
+ *
+ * @author diego
+ */
+public class Recolector_Oro implements Edificacion{
+
+    @Override
+    public String generar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
