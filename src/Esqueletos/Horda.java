74815eccce1f8a63fa16866b3205a34358979dd8
Diego Vega <00148816@uca.edu.sv>
2018-06-03 16:47:54 -0600

    Modificacion de metodos agregados, ignorando metodos que no seran utilizados para la clase

--- src/Esqueletos/Horda.java
+++ src/Esqueletos/Horda.java
@@ -10,6 +10,42 @@ package Esqueletos;
  *
  * @author diego
  */
-public class Horda {
+public class Horda implements Esqueleto{
+
+    //METODOS MILICIA.
+    @Override
+    public String M_entrenar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String M_atacar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String M_defender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    //METODOS EDIFICACION.
+    @Override
+    public String E_generar() {
+        return null;
+    }
+
+    @Override
+    public String E_recolectar() {
+        return null;
+    }
+    //METODOS VEHICULO.
+    @Override
+    public String V_construccion() {
+        return null;
+    }
+
+    @Override
+    public String V_atacar() {
+        return null;
+    }
     
 }
--- src/Esqueletos/VLigero_E.java
+++ src/Esqueletos/VLigero_E.java
@@ -10,6 +10,41 @@ package Esqueletos;
  *
  * @author diego
  */
-public class VLigero_E {
+public class VLigero_E implements Esqueleto{
+
+    @Override
+    public String M_entrenar() {
+        return null;
+    }
+
+    @Override
+    public String M_atacar() {
+        return null;
+    }
+
+    @Override
+    public String M_defender() {
+        return null;
+    }
+
+    @Override
+    public String E_generar() {
+        return null;
+    }
+
+    @Override
+    public String E_recolectar() {
+        return null;
+    }
+
+    @Override
+    public String V_construccion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String V_atacar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
