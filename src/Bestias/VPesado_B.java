50649ac426a79144093053c857a4097373594030
Diego Vega <00148816@uca.edu.sv>
2018-06-03 16:03:00 -0600

    Creacion de la clase que implementara los metodos de su interfaz correspondiente

---
+++ src/Bestias/VPesado_B.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bestias;
+
+import Humanos.*;
+
+/**
+ *
+ * @author diego
+ */
+public class VPesado_B {
+    
+}
