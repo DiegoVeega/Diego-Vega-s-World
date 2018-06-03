303f1dbe1080f0a21289d207b7858e8b02f66864
Diego Vega <00148816@uca.edu.sv>
2018-06-03 16:00:38 -0600

    Creacion de la interfaz 'Bestia' para manejar cada metodo de los productos concretos para esta especie

---
+++ src/Bestias/Bestia.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bestias;
+
+/**
+ *
+ * @author diego
+ */
+public interface Bestia {
+    //METODOS MILICIA:
+    String M_entrenar();
+    String M_atacar();
+    String M_defender();
+    //METODOS EDIFICACION:
+    String E_generar();
+    String E_recolectar();
+    //METODOS VEHICULO:
+    String V_construccion();
+    String V_atacar();
+}
