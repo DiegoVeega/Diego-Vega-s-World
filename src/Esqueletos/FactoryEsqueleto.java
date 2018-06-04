ee5a5397d08178a4889f82eab5e0e165c066584d
Diego Vega <00148816@uca.edu.sv>
2018-06-04 15:34:34 -0600

    Especificando pasos a seguir para el metodo 'getFabrica'

--- src/Esqueletos/FactoryEsqueleto.java
+++ src/Esqueletos/FactoryEsqueleto.java
@@ -3,6 +3,7 @@ package Esqueletos;
 import Bestias.Bestia;
 import Factorys.AbstractFactory;
 import Humanos.Humano;
+import java.util.Scanner;
 
 /**
  *
@@ -11,17 +12,63 @@ import Humanos.Humano;
 public class FactoryEsqueleto implements AbstractFactory{
 
     @Override
-    public Bestia getBestia() {
+    public Bestia getBestia(int tipo) {
         return null;
     }
 
     @Override
-    public Esqueleto getEsqueleto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Esqueleto getEsqueleto(int tipo) {
+        int tecla;
+        Scanner leer=new Scanner(System.in);
+        switch(tipo){
+            case 1:
+                System.out.println("\n\t\tQue tipo de recolector desea? ");
+                System.out.println("\n\t\t1. Recolector de Plata.");
+                System.out.println("\n\t\t2. Recolector de Oro.");
+                System.out.println("\n\t\t3. Recolector de Diamante.");
+                System.out.print("Tu eleccion: ");
+                tecla=leer.nextInt();
+                switch(tecla){
+                    case 1:
+                        return new Recolector_Plata_E();
+                    case 2:
+                        return new Recolector_Oro_E();
+                    case 3:
+                        return new Recolector_Diamante_E();
+                }
+                break;
+            case 2:
+                System.out.println("\n\t\tA que miembro de milicia desea? ");
+                System.out.println("\n\t\t1. horda Esqueletica.");
+                System.out.println("\n\t\t2. Esqueleto Colosal.");
+                System.out.print("Tu eleccion: ");
+                tecla=leer.nextInt();
+                switch(tecla){
+                    case 1:
+                        return new Horda();
+                    case 2:
+                        return new EsqueletoColosal();
+                }
+                break;
+            case 3:
+                System.out.println("\n\t\tQue tipo de Vehiculo desea? ");
+                System.out.println("\n\t\t1. Vehiculo ligero.");
+                System.out.println("\n\t\t2. Vehiculo Pesado.");
+                System.out.print("Tu eleccion: ");
+                tecla=leer.nextInt();
+                switch(tecla){
+                    case 1:
+                        return new VLigero_E();
+                    case 2:
+                        return new VPesado_E();
+                }
+                break;
+        }
+        return null;
     }
 
     @Override
-    public Humano getHumano() {
+    public Humano getHumano(int tipo) {
         return null;
     }
     
