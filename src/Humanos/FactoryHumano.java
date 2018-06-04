7991d3f04169a3e4789bacf5c4a60d0ba5e62f9b
Diego Vega <00148816@uca.edu.sv>
2018-06-04 15:34:23 -0600

    Especificando pasos a seguir para el metodo 'getFabrica'

--- src/Humanos/FactoryHumano.java
+++ src/Humanos/FactoryHumano.java
@@ -3,6 +3,7 @@ package Humanos;
 import Bestias.Bestia;
 import Esqueletos.Esqueleto;
 import Factorys.AbstractFactory;
+import java.util.Scanner;
 
 /**
  *
@@ -11,18 +12,63 @@ import Factorys.AbstractFactory;
 public class FactoryHumano implements AbstractFactory{
 
     @Override
-    public Bestia getBestia() {
+    public Bestia getBestia(int tipo) {
         return null;
     }
 
     @Override
-    public Esqueleto getEsqueleto() {
+    public Esqueleto getEsqueleto(int tipo) {
         return null;
     }
 
     @Override
-    public Humano getHumano() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Humano getHumano(int tipo) {
+        int tecla;
+        Scanner leer=new Scanner(System.in);
+        switch(tipo){
+            case 1:
+                System.out.println("\n\t\tQue tipo de recolector desea? ");
+                System.out.println("\n\t\t1. Recolector de Madera.");
+                System.out.println("\n\t\t2. Recolector de Piedra.");
+                System.out.println("\n\t\t3. Recolector de Hierro.");
+                System.out.print("Tu eleccion: ");
+                tecla=leer.nextInt();
+                switch(tecla){
+                    case 1:
+                        return new Recolector_Madera();
+                    case 2:
+                        return new Recolector_Piedra();
+                    case 3:
+                        return new Recolector_Hierro();
+                }
+                break;
+            case 2:
+                System.out.println("\n\t\tA que miembro de milicia desea? ");
+                System.out.println("\n\t\t1. Soldados.");
+                System.out.println("\n\t\t2. Especialista.");
+                System.out.print("Tu eleccion: ");
+                tecla=leer.nextInt();
+                switch(tecla){
+                    case 1:
+                        return new Soldados();
+                    case 2:
+                        return new Especialista();
+                }
+                break;
+            case 3:
+                System.out.println("\n\t\tQue tipo de Vehiculo desea? ");
+                System.out.println("\n\t\t1. Vehiculo ligero.");
+                System.out.println("\n\t\t2. Vehiculo Pesado.");
+                System.out.print("Tu eleccion: ");
+                tecla=leer.nextInt();
+                switch(tecla){
+                    case 1:
+                        return new VLigero_H();
+                    case 2:
+                        return new VPesado_H();
+                }
+                break;
+        }
+        return null;
     }
-    
 }
