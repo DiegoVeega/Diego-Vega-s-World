f87e1648a5d8ee1cb63ea338a02142e43ffa8476
Diego Vega <00148816@uca.edu.sv>
2018-06-10 22:49:36 -0600

    Agregando metodos faltantes

--- src/Humanos/Humano.java
+++ src/Humanos/Humano.java
@@ -13,17 +13,24 @@ import java.util.ArrayList;
  */
 public interface Humano {
     //METODOS MILICIA:
-    String M_entrenar();
-    String M_atacar();
-    String M_defender();
+    ArrayList<Soldados> M_entrenar();
+    void M_atacar(ArrayList<Soldados> Tribu);
+    void M_defender(ArrayList<Soldados> Tribu);
+    
+    ArrayList<Especialista> M_entrenarE();
+    void M_atacarE(ArrayList<Especialista> Especialista);
+    void M_defenderE(ArrayList<Especialista> Especialista);
     //METODOS EDIFICACION:
     int E_generar();
     ArrayList<Integer> E_recolectar(int a,ArrayList<Integer> Centro);
     //METODOS VEHICULO:
-    String V_construccion();
-    String V_atacar();
+    ArrayList<VLigero_H> V_construccion();
+    void V_atacar(ArrayList<VLigero_H> VLigero_H);
+    
+    ArrayList<VPesado_H> V_construccionP();
+    void V_atacarP(ArrayList<VPesado_H> VPesado_H);
     //CENTRO DE MANDO
     ArrayList<Integer> generarCH();
-    void mejorarCH(int num,ArrayList<Integer> Centro);
+    ArrayList<Integer> mejorarCH(int num,ArrayList<Integer> Centro);
     void mostrarCH(ArrayList<Integer> Centro);
 }
