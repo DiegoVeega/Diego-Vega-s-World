f11a27651135ea938e09ff199e5d3893d8728e9b
Diego Vega <00148816@uca.edu.sv>
2018-06-10 22:30:19 -0600

    Agregando metodos faltantes

--- src/Esqueletos/Esqueleto.java
+++ src/Esqueletos/Esqueleto.java
@@ -13,17 +13,24 @@ import java.util.ArrayList;
  */
 public interface Esqueleto {
     //METODOS MILICIA:
-    String M_entrenar();
-    String M_atacar();
-    String M_defender();
+    ArrayList<Horda> M_entrenar();
+    void M_atacar(ArrayList<Horda> Horda);
+    void M_defender(ArrayList<Horda> Horda);
+    
+    ArrayList<EsqueletoColosal> M_entrenarE();
+    void M_atacarE(ArrayList<EsqueletoColosal> EsqueletoColosal);
+    void M_defenderE(ArrayList<EsqueletoColosal> EsqueletoColosal);
     //METODOS EDIFICACION:
     int E_generar();
     ArrayList<Integer> E_recolectar(int a,ArrayList<Integer> Centro);
     //METODOS VEHICULO:
-    String V_construccion();
-    String V_atacar();
+    ArrayList<VLigero_E> V_construccion();
+    void V_atacar(ArrayList<VLigero_E> VLigero_E);
+    
+    ArrayList<VPesado_E> V_construccionP();
+    void V_atacarP(ArrayList<VPesado_E> VPesado_E);
     //CENTRO DE MANDO
     ArrayList<Integer> generarCE();
-    void mejorarCE(int num,ArrayList<Integer> Centro);
+    ArrayList<Integer> mejorarCE(int num,ArrayList<Integer> Centro);
     void mostrarCE(ArrayList<Integer> Centro);
 }
